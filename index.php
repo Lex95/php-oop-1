618a1c07a7ab4e9a23243744d4ca2890d106f729
Lex95 <andrea.lega1995@gmail.com>
2021-06-14 15:29:52 +0200

    definizione della classe e di due oggetti

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
+class Movie {
+    public $nome;
+    public $anno;
+    public $regista;
 
+    function __construct($nome, $anno, $regista = NULL) {
+        $this->nome = $nome;
+        $this->anno = $anno;
+        $this->regista = $regista;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getAnno(){
+        return $this->anno;
+    }
+    public function getRegista(){
+        return $this->regista;
+    }
+}
+
+$primoFilm = new Movie("Fast and Furious", 2001, "Rob Cohen");
+$secondoFilm = new Movie("Avengers", 2012);
 
 ?>

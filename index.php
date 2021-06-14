53a9d95ec66ff79bd3f19dcf0030d09cd6c8dc67
Lex95 <andrea.lega1995@gmail.com>
2021-06-14 15:33:56 +0200

    risoluzione esercizio

--- index.php
+++ index.php
@@ -26,3 +26,22 @@ $primoFilm = new Movie("Fast and Furious", 2001, "Rob Cohen");
 $secondoFilm = new Movie("Avengers", 2012);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $primoFilm->getNome() ?></h1>
+    <p><?php echo $primoFilm->getAnno() ?></p>
+    <p><?php echo $primoFilm->getRegista() ?></p>
+
+    <h1><?php echo $secondoFilm->getNome() ?></h1>
+    <p><?php echo $secondoFilm->getAnno() ?></p>
+    <p><?php echo $secondoFilm->getRegista() ?></p>
+</body>
+</html>
